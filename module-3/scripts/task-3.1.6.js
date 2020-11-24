27c2880e642ba15836fcf208da8c08177e81f641
EdP99 <74243502+EdP99@users.noreply.github.com>
2020-11-24 20:15:07 +0000

    Create task-3.1.6.js
    Used concatenation to add var strings together and determine final bill. Output to console.log.

---
+++ module-3/scripts/task-3.1.6.js
@@ -0,0 +1,6 @@
+var costofmeal= 40;
+var servicecharge= costofmeal / 100 * 12.5;
+var totalcost= costofmeal + servicecharge;
+var finalbill= 'The total cost of the meal, with tip of ' + '£' + servicecharge + ', is ' + '£' + totalcost + ".";
+
+console.log (finalbill);
