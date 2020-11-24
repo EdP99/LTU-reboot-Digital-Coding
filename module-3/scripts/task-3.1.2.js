7e29e579544d4d5e5850fdb6ef67c8da2dbd2198
EdP99 <74243502+EdP99@users.noreply.github.com>
2020-11-24 17:52:10 +0000

    Update task-3.1.2.js
    Update content and syntax for alert, console.log and doc.write

--- module-3/scripts/task-3.1.2.js
+++ module-3/scripts/task-3.1.2.js
@@ -1,12 +1,9 @@
 
 alert('Its a long road!');
 
-console.log('Tricky business!');//Difficult to get to the bottom of this.
+console.log('Tricky business!');
 
-document.write('Hello from Doc write!');
-/*
-Trying to get me head around the use of comments and variables in JS
-*/
+document.write('Hello from Doc write');
 
 
 
