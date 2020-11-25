4f0699187656a41ab34a87ccf7b75dbfa97e4675
EdP99 <74243502+EdP99@users.noreply.github.com>
2020-11-25 11:14:18 +0000

    Update 3.2.3
    add Fullname function

---
+++ module-3/scripts/task-3.2.3.js
@@ -0,0 +1,11 @@
+function fullName( firstName, lastName ) {
+
+    return firstName + lastName;
+}
+
+
+console.log( fullName( 'Ed', 'Partridge') );
+
+var nameOfPerson = fullName ( 'Ed', 'Partridge');
+
+console.log( nameOfPerson );
