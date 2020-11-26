38188eaec9296fe54d23bdef824275f6365cea1e
EdP99 <74243502+EdP99@users.noreply.github.com>
2020-11-26 22:08:10 +0000

    Update task check
    scan for incorrect code

--- module-1/task-check.js
+++ module-1/task-check.js
@@ -1,12 +1,15 @@
 
-for (var index = 1; index < 100; index++) {
-    // if (index %3 === 0); {
-    // console.log(Fizz);
-    // }
-    // else if (index %5 === 0); {
-    // console.log(Buzz);
-    // }
-    // else {
-    // console.log(index);
-    // }
+for (var index = 0; index < 100; index++) {
+    if (index %3 === 0); 
+        {
+        console.log('Fizz');
+        }
+    if (index %5 === 0); 
+        {
+        console.log('Buzz');
+        }
+    {
+        console.log[index];
+        }
+
 }
