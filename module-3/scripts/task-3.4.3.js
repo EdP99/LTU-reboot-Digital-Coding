f2ca6f6070582c476a1dba280a7fc11f77af35f1
EdP99 <74243502+EdP99@users.noreply.github.com>
2020-11-26 10:45:14 +0000

    Create task-3.4.3.js
    print nice food list using doc.write

---
+++ module-3/scripts/task-3.4.3.js
@@ -0,0 +1,8 @@
+var nicefood = ['rice', 'chicken', 'beans', 'chocolate', 'crisps', 'ice-cream']; 
+
+for (var index = 0; index < nicefood.length; index++)
+
+{
+
+    document.write( nicefood [index] );
+}
