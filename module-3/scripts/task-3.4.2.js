c18ac4abec6133021f0751d3b9d9d71ea581e6bd
EdP99 <74243502+EdP99@users.noreply.github.com>
2020-11-26 20:26:38 +0000

    Update task 3.4.2
    Update niceFood array list and change for loop values

--- module-3/scripts/task-3.4.2.js
+++ module-3/scripts/task-3.4.2.js
@@ -1,8 +1,8 @@
 
 var niceFood = ['rice', 'chicken', 'beans', 'chocolate', 'crisps', 'ice-cream']; 
 
-for(var index = 0; index < niceFood.length; index++ ) {
-
+for(var index = 0; index = niceFood.length; index++)
+{
     console.log( niceFood[index] );
 
 }
