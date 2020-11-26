408434f3e2b4bd9a3789ff0f2258f4fa3f3c3de6
EdP99 <74243502+EdP99@users.noreply.github.com>
2020-11-26 22:24:17 +0000

    Create task 3.4.5

---
+++ module-3/scripts/task-3.4.5.js
@@ -0,0 +1,32 @@
+var recipe = {
+    recipeTitle: 'Chicken rice',
+    servings: 6,
+    ingredients: [
+        '1 tbsp olive oil',
+        'chicken', 
+        'onion', 
+        'rice', 
+        'peppers' 
+        ],
+    directions: ['Fry onion, brown chicken, add peppers etc'
+                ], 
+    letsCook: function () {
+        console.log("I'm hungry, let's cook " + recipe.recipeTitle); }     
+}
+
+recipe.letsCook ();
+
+
+    // {
+    //     console.log( "Title " + recipe.recipeTitle );
+    //     console.log( "Servings " + recipe.servings );
+    //     console.log( "Ingredients " + recipe.ingredients );
+    //     console.log( "Directions " + recipe.directions );
+    // }
+
+    var ingredients = recipe.ingredients;
+
+    for (var index = 0; index < ingredients.length; index++ ) {
+
+        console.log( ingredients [index] );
+    }
