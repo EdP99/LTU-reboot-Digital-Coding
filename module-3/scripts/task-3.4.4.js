546586a735621fd055c5666ea4b6a12c864d3b2d
EdP99 <74243502+EdP99@users.noreply.github.com>
2020-11-26 21:58:45 +0000

    Update task 3.4.4
    revise code to output recipe

--- module-3/scripts/task-3.4.4.js
+++ module-3/scripts/task-3.4.4.js
@@ -1,30 +1,29 @@
 var recipe = {
     recipeTitle: 'Chicken rice',
-    servings: 6;
+    servings: 6,
     ingredients: [
         '1 tbsp olive oil',
         'chicken', 
         'onion', 
         'rice', 
-        'peppers',
-    ],
-    directions: [
-        "Fry onion",
-        "brown chicken",
-        'Add peppers',
-         "<br>" ]
-};
-    {
-        // console.log(recipe.recipeTitle);
-        // console.log(recipe.servings);
-        // console.log(recipe.ingredients);
-        // console.log(recipe.directions);
-
-
-        var ingredients = recipe.ingredients
-
-        for(var index = 0; index <= ingredients.length; index++) {
-
-            document.write( ingredients[index] );
-        }
+        'peppers' 
+        ],
+    directions: 'Fry onion, brown chicken, add peppers etc'
 }
+
+
+    // {
+    //     console.log( "Title " + recipe.recipeTitle );
+    //     console.log( "Servings " + recipe.servings );
+    //     console.log( "Ingredients " + recipe.ingredients );
+    //     console.log( "Directions " + recipe.directions );
+    // }
+
+    var ingredients = recipe.ingredients;
+
+    for (var index = 0; index < ingredients.length; index++ ) {
+
+        console.log( ingredients [index] );
+    }
+
+    
