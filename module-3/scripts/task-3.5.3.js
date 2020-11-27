c63f6809461ab469c818c6cc1cd2dd6db201e2cd
EdP99 <74243502+EdP99@users.noreply.github.com>
2020-11-27 15:00:08 +0000

    Update task-3.5.3.js

--- module-3/scripts/task-3.5.3.js
+++ module-3/scripts/task-3.5.3.js
@@ -60,24 +60,13 @@ let shoppingCart = [
     price:.4
   }];
 
-  function discountFood ( arr ) { 
+  function discount ( arr, discountAmount, type ) { 
 
     let totalPrice = 0; 
 
         for  ( let index = 0; index < arr.length; index++ ) {
 
-        if ( arr[index].type === 'food' ) {
-
-          let discount =  ( arr[index].price * 20 ) / 100;
-
-          totalPrice = totalPrice + ( arr [index].price - discount ) * arr[index].quantity;
-
-        } else {
-
-            totalPrice = totalPrice + ( arr[index].price * arr[index].quantity )
-
-        }
-
+        
         
 
     }
@@ -86,6 +75,6 @@ let shoppingCart = [
 
   }
 
- 
+  let shopping = discount( shoppingCart, 15, 'food' );
 
-  console.log( discountFood ( shoppingCart ) );
+  console.log( shopping );
