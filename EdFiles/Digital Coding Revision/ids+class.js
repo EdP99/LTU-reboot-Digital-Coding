b285fabcfee206a6777a63f9e9a289af277a156d
EdP99 <74243502+EdP99@users.noreply.github.com>
2021-01-19 13:35:55 +0000

    Update ids+class.js
    styles added

---
+++ EdFiles/Digital Coding Revision/ids+class.js
@@ -0,0 +1,31 @@
+body {
+    font-family: helvetica;
+    font-weight: normal;
+    font-size: 12px;
+    color: #000000;
+}
+background {
+    color: rgb(235, 235, 224);
+}
+
+#head {
+    font-size: 20px;
+    color: #33bbff;
+    font-family: helvetica neue;
+    font-weight: bold;
+}
+
+.subhead {
+    font-size: 15px;
+    color: #e68a00;
+    font-family: helvetica neue;
+    font-weight: italic;
+}
+
+#video {
+    font-size: 15px;
+    color: #ff0000;
+    font-family: helvetica;
+    font-weight: bold;
+}
+
